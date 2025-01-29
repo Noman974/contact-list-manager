645cd67866d9155a5adeaa9663e9a1b0a9db4860
Hassaan <Hassaan@mail.com>
2025-01-29 10:00:14 +0300

    Email validation has been added!

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Regexp, Length
+from wtforms.validators import DataRequired, Regexp, Length, Email
 
 class ContactForm(FlaskForm):
     name = StringField('Name', 
@@ -12,7 +12,11 @@ class ContactForm(FlaskForm):
                         Length(min=8, max=8, message="Phone number must be exactly 8 digits"), 
                         Regexp(r'^\d{8}$', message="Phone number must contain only digits")])
 
-    email = StringField('Email')
+    email = StringField('Email', 
+                        validators=[DataRequired(), 
+                        Email(message="Invalid email address")])
+
+
     type = SelectField('Type', 
                       choices=[('Personal', 'Personal'), 
                               ('Work', 'Work'), 
