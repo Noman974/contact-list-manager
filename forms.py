17fcb1d5371f06365495d66badd1d4fc94d11dcb
Hassaan <Hassaan@mail.com>
2025-01-29 09:54:58 +0300

    Invalid name characters issue has been solved

--- forms.py
+++ forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Regexp
 
 class ContactForm(FlaskForm):
-    name = StringField('Name')
+    name = StringField('Name', 
+                        validators=[DataRequired(), 
+                        Regexp(r'^[A-Za-z\s]+$', message="Name must only contain letters and spaces")])
+
     phone = StringField('Phone')
     email = StringField('Email')
     type = SelectField('Type', 
